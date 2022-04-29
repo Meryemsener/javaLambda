80862b9bdb21ff67274c68c04fa2402606817f12
Meryem Aydin <meryemaydinsener@gmail.com>
2022-04-29 18:26:29 +0200

    4; gun

--- src/Lambda04.java
+++ src/Lambda04.java
@@ -1,17 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Lambda04 {
-    /*
-    TASK :
-    fields --> Universite (String)
-               bolum (String)
-               ogrcSayisi (int)
-               notOrt (int)
-               olan POJO clas craete edip main method içinde 5 arklı obj'den List create ediniz.
-     */
+
     public static void main(String[] args) {
+
+        Universite bogazici=new Universite("bogazici","matematik",571,89);
+        Universite itu=new Universite("istanbul teknik ","matematik",622,82);
+        Universite istanbul=new Universite("istanbul ","hukuk",1453,71);
+        Universite marmara=new Universite("Marmara univeristesi","bilgisayar muhendisi",1071,77);
+        Universite ytu=new Universite("yildiz teknik","gemi",333,74);
+
+        List<Universite> unv=new ArrayList<>(Arrays.asList(bogazici,itu,istanbul,marmara,ytu));
+
+        System.out.println(notOrt74Buyuk(unv));
+
 
     }
 
     //task 01--> notOrt'larinin 74' den buyuk oldg kontrol eden pr create ediniz.
+    public static boolean notOrt74Buyuk(List<Universite> unv) {
+
+        return unv.
+                stream().
+                anyMatch(t->t.getNotOrt()>74);//hic var mi bak
+        //allMatch yazarsan hepsi buyuk mu ona bakar, bir tane bile kucukse false doner
+    }
 
 
     //task 02-->ogrc sayilarinin   110 den az olmadigini  kontrol eden pr create ediniz.
