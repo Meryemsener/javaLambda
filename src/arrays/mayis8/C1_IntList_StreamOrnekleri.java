da1cf723015eac70d6b5ea8f967d7c016599374e
Meryem Aydin <meryemaydinsener@gmail.com>
2022-05-09 01:17:20 +0200

    lambda

--- src/arrays/mayis8/C1_IntList_StreamOrnekleri.java
+++ src/arrays/mayis8/C1_IntList_StreamOrnekleri.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 
 public class C1_IntList_StreamOrnekleri {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(Arrays.asList(-5,-8,-2,-12,0,1,12,5,6,9,15,8));
+        List<Integer> list = new ArrayList<>(Arrays.asList(-5, -8, -2, -12, 0, 1, 12, 5, 6, 9, 15, 8));
 
         System.out.println(list);
         System.out.println();
@@ -28,31 +28,45 @@ public class C1_IntList_StreamOrnekleri {
         System.out.println();
         kupler(list);
         System.out.println();
-        kareleri1( list);
+        kareleri1(list);
         System.out.println();
         eletoplam(list);
         System.out.println();
         peeknegatif(list);
+        System.out.println();
+        bestenBuyukKontrol(list);
+        System.out.println();
+        tumElSifirdanKucukmu(list);
+        System.out.println();
+        yuzdenKucuk(list);
+        System.out.println();
+        ilkBesElemaniTopla(list);
+        System.out.println();
+        sonBesElemaniTopla(list);
 
 
     }
+
     // S1:listi aralarinda bosluk birakarak yazdiriniz //
-    public static void bosluk(List<Integer> list){
-        list.stream().forEach(t-> System.out.print(t+" "));
+    public static void bosluk(List<Integer> list) {
+        list.stream().forEach(t -> System.out.print(t + " "));
     }
-    public static void yazdir(int t){ System.out.print(t+" "); }
+
+    public static void yazdir(int t) {
+        System.out.print(t + " ");
+    }
 
     //S2: sadece negatif olanlari yazdir
 
-    public static void negatif(List<Integer> list){
-        list.stream().filter(t->t<0).forEach(C1_IntList_StreamOrnekleri::yazdir);
+    public static void negatif(List<Integer> list) {
+        list.stream().filter(t -> t < 0).forEach(C1_IntList_StreamOrnekleri::yazdir);
     }
 
     //S3: pozitif olanlardan yeni bir liste olustur
 
-    public static void pozitif(List<Integer> list){
+    public static void pozitif(List<Integer> list) {
         list.stream().
-                filter(t->t>0).
+                filter(t -> t > 0).
                 collect(Collectors.toList()).
                 forEach(C1_IntList_StreamOrnekleri::yazdir);
 
@@ -60,40 +74,40 @@ public class C1_IntList_StreamOrnekleri {
 
 
     // S4: list in elemanlarin karelerinden yeni bir list olusturalim
-    public static void kareleri(List<Integer> list){
-        list.stream().map(t->t*t).forEach(C1_IntList_StreamOrnekleri::yazdir);
+    public static void kareleri(List<Integer> list) {
+        list.stream().map(t -> t * t).forEach(C1_IntList_StreamOrnekleri::yazdir);
     }
 
     //S5 : list in elemanlarin karelerinden tekrarsiz yeni bir list olusturalim
-    public static void tekrarsiz(List<Integer> list){
-        list.stream().map(t->t*t).distinct().forEach(C1_IntList_StreamOrnekleri::yazdir);
+    public static void tekrarsiz(List<Integer> list) {
+        list.stream().map(t -> t * t).distinct().forEach(C1_IntList_StreamOrnekleri::yazdir);
     }
 
     //S6: listin elemanlarini kucukten buyuge siralayalim
-    public static void kucukbuyuk(List<Integer> list){
+    public static void kucukbuyuk(List<Integer> list) {
         list.stream().sorted().forEach(C1_IntList_StreamOrnekleri::yazdir);
     }
 
     //S7: listin elemanlarini buyukten kucuge siralayalim
-    public static void buyukkucuk(List<Integer> list){
+    public static void buyukkucuk(List<Integer> list) {
         list.stream().sorted(Comparator.reverseOrder()).forEach(C1_IntList_StreamOrnekleri::yazdir);
     }
 
     // S8: list pozitif elemanlari icn kuplerini bulup birler basamagi 5 olanlardan yeni bir list olusturalim
-    public static void kupler(List<Integer> list){
-        list.stream().map(t->t*t*t).
-                filter(t->t%10==5).
+    public static void kupler(List<Integer> list) {
+        list.stream().map(t -> t * t * t).
+                filter(t -> t % 10 == 5).
                 forEach(C1_IntList_StreamOrnekleri::yazdir);
     }
 
     //S9: list pozitif elemanlari icn karelerini bulup birler basamagi 5 olmayanlardan yeni bir list olusturalim
 
-    public static void kareleri1(List<Integer> list){
-        list.stream().filter(t->t>0).map(t->t*t).filter(t->t%10!=5).forEach(C1_IntList_StreamOrnekleri::yazdir);
+    public static void kareleri1(List<Integer> list) {
+        list.stream().filter(t -> t > 0).map(t -> t * t).filter(t -> t % 10 != 5).forEach(C1_IntList_StreamOrnekleri::yazdir);
     }
 
     // S10 :list elemanlarini toplamini bulalim
-    public static void eletoplam(List<Integer> list){
+    public static void eletoplam(List<Integer> list) {
         System.out.print(list.stream().reduce(0, (t, u) -> t + u));
     }
 /*
@@ -103,9 +117,9 @@ public class C1_IntList_StreamOrnekleri {
  */
 
     // S11 : peek ornegi cozelim - negatiflerin karelerinden list olusturalim
-    public static void peeknegatif(List<Integer> list){
-        list.stream().filter(t->t<0).peek(e -> System.out.println("Filtered value: " + e))
-                .map(t->t*t)
+    public static void peeknegatif(List<Integer> list) {
+        list.stream().filter(t -> t < 0).peek(e -> System.out.println("Filtered value: " + e))
+                .map(t -> t * t)
                 .peek(e -> System.out.println("Mapped value: " + e))
                 .collect(Collectors.toList());
         //forEach(C1_IntList_StreamOrnekleri::yazdir);
@@ -113,28 +127,42 @@ public class C1_IntList_StreamOrnekleri {
     }
 
 
-
     // S12 : listeden 5 den buyuk  sayi var mi?
-
+    public static void bestenBuyukKontrol(List<Integer> list) {
+        System.out.println(list.stream().anyMatch(t -> t > 5));
+    }
 
 
     // S13 : listenin tum elemanlari sifirdan kucuk mu?
-
+    public static void tumElSifirdanKucukmu(List<Integer> list){
+        System.out.println(list.stream().allMatch(t -> t < 0));
+    }
 
 
     // S14: listenin 100 e esit elemani yok mu ?
-
+    public static void yuzdenKucuk(List<Integer> list){
+        System.out.println(list.stream().noneMatch(t -> t ==100));
+    }
 
 
     // S15: listenin sifira esit elemani yok mu?
-
-
+    public static void sifiraEsit(List<Integer> list){
+        System.out.println(list.stream().noneMatch(t -> t ==0));
+    }
 
     // S16:  listenin ilk 5 elemanini topla?
 
+    public static void ilkBesElemaniTopla(List<Integer> list){
+        System.out.println(list.stream().limit(5).reduce(Integer::sum).orElseThrow());
+
+    }
 
 
     //S17: listenin son bes elemaninin  listele
 
+    public static void sonBesElemaniTopla(List<Integer> list){
+        list.stream().skip(list.size()-5).forEach(t-> System.out.print(t+" "));
+
+    }
 
 }
--- src/arrays/mayis8/C6_Lambdada_ForLoop_Aralik_Kontrolu.java
+++ src/arrays/mayis8/C6_Lambdada_ForLoop_Aralik_Kontrolu.java
@@ -1,45 +1,99 @@
 package arrays.mayis8;
 
+import java.util.stream.IntStream;
+
 public class C6_Lambdada_ForLoop_Aralik_Kontrolu {
     public static void main(String[] args) {
+        sirala(30);
+        System.out.println();
+        ikiDEgerArasiniTopla(3,5);
+        System.out.println();
+        ortalamaAlma(30,40);
+        System.out.println();
+        sekizeBolunebilen(325,468);
+        System.out.println();
+        sekizeBolunebilenYazdir(325,468);
+        System.out.println();
+        sekizeBolunebilenTopla(325,468);
+        System.out.println();
+        tekSayilarinToplami(7,15);
+        System.out.println();
+        ilkOnEleman(1,20);
+        System.out.println();
+        yirmibirdenOnEleman(21);
+        System.out.println();
+        yirmibirdenOnElemanTopla(21);
+
 
 
     }
 
     // S1:1 den 30 kadar olan sayilari (30 dahil degil) 1 2 3 .... seklinde siralayalim (for loopsuz)
     //range(int startInclusive, int endExclusive)
-
+public static void sirala(int x){
+   IntStream.range(1, 30).sorted().forEach(t -> System.out.print(t + " "));
+}
 
     //S2 1 den 30 kadar olan sayilari (30 dahil ) 1 2 3 .... seklinde siralayalim (for loopsuz)
     //rangeClosed(int startInclusive, int endInclusive)
-
+    public static void sirala30Dahil(int x){
+        IntStream.rangeClosed(1, 30).sorted().forEach(t -> System.out.print(t + " "));
+    }
 
     //S3 Istenen iki deger(dahi) arasindaki sayilari toplayalim
+    public static void ikiDEgerArasiniTopla(int x, int t){
+        System.out.println(IntStream.rangeClosed(x, t).reduce(0, (a, b) -> a + b));
 
+    }
 
     //S4: 30 ile 40 arasindaki sayilarin (dahi) ortalamasini bulalim
+    public static void ortalamaAlma(int x, int t){
+        System.out.println(IntStream.rangeClosed(30, 40).average().orElseThrow());
+
+    }
 
 
     //S5: 325 ile 468 arasinda 8 e bolunen kac sayi vardir?
+    public static void sekizeBolunebilen(int x, int a){
+        System.out.println(IntStream.rangeClosed(x, a).filter(t -> t % 8 == 0).count());
+
+    }
 
     //S6: 325 ile 468 arasinda 8 bolunen sayilari yazdiralim
+    public static void sekizeBolunebilenYazdir(int x, int a){
+        IntStream.rangeClosed(x, a).filter(t -> t % 8 == 0).forEach(t-> System.out.print(t+" "));
 
+    }
 
     // S7:325 ile 468 arasinda 8 bolunen sayilarin toplamini bulalim
 
+    public static void sekizeBolunebilenTopla(int x, int a){
+        System.out.println(IntStream.rangeClosed(x, a).filter(t -> t % 8 == 0).sum());
+
+    }
+
 
     // S8: 7ile 15 arasindaki tek sayilarin carpimini bulalim
+    public static void tekSayilarinToplami(int a,int b){
+        System.out.println(IntStream.range(a+1, b).filter(t->t%2!=0).reduce(1, (x, y) -> x * y));
+    }
 
 
     //S9: pozitif tek sayilarin ilk 10 elemanin yazdiralim
-
+    public static void ilkOnEleman(int a,int b){
+        IntStream.rangeClosed(a,b).filter(t->t%2!=0).limit(20).forEach(t-> System.out.print(t+" "));
+    }
 
     //S10: 21 den baslayan 7 nin katlarinin tek olanlari ilk 10 teriminin yaziralim
-
+    public static void yirmibirdenOnEleman(int a){
+        IntStream.iterate(21, t -> t + 7).filter(t->t%7==0 && t%2!=0).limit(10).forEach(t-> System.out.print(t+" "));
+    }
 
 
     //S11: 21 den baslayan 7 nin katlarinin ilk 20 teriminin toplayalim
-
+    public static void yirmibirdenOnElemanTopla(int a){
+        System.out.println(IntStream.iterate(21, t -> t + 7).filter(t -> t % 7 == 0).limit(20).reduce(Math::addExact));
+    }
 
 //iterate bize sayilari istedigimiz sekilde yineletmeyi saglar yani buradaki ornekte 7ser7ser artmayi saglar
 //iterate icin bir baslangic degeri girilmeli ve artisin nasil olacagi belirtilmeli nerede biteceginide
