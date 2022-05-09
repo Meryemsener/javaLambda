fee9fded1e67e2ccd4bd684d45cd1224d79964ab
Meryem Aydin <meryemaydinsener@gmail.com>
2022-05-10 00:20:21 +0200

    lambda bitttii

--- src/arrays/mayis8/C4_Method_Refarance_Int.java
+++ src/arrays/mayis8/C4_Method_Refarance_Int.java
@@ -11,19 +11,53 @@ import java.util.List;
 //Math classi sayesinde de sum vb methodlar kullanabiliriz
 public class C4_Method_Refarance_Int {
     public static void main(String[] args) {
-        List<Integer> intL= Arrays.asList(-2,-8,-5,0,2,5,6,7,15,6,8);
+        List<Integer> intL = Arrays.asList(-2, -8, -5, 2, 5, 6, 7, 15, 6, 8);
+        teklerinArasinaBosluklu(intL);
+        System.out.println();
+        ciftlerinCarpimi(intL);
+        System.out.println();
+        negatiflerinKareToplami(intL);
+        System.out.println();
+        pozKupleri(intL);
 
 
     }
-    // S1: tekleri aralarinda bir bosluk birakarak yazdiralim
 
+    public static void yazdir(int a) {
+        System.out.print(a + " ");
+    }
+
+    // S1: tekleri aralarinda bir bosluk birakarak yazdiralim
+    public static void teklerinArasinaBosluklu(List<Integer> intL) {
+        intL.stream().filter(t -> t % 2 != 0).forEach(C4_Method_Refarance_Int::yazdir);
+    }
 
     // S2: ciftlein carpimini bulalim
-
+    public static void ciftlerinCarpimi(List<Integer> intL) {
+        System.out.println(intL.stream().filter(t -> t % 2 == 0).
+                reduce(Math::multiplyExact).
+                orElseThrow());
+    }
 
     // S3: negatiflerin kare toplamlarini  bulalim
-
+    public static void negatiflerinKareToplami(List<Integer> intL) {
+        System.out.println(intL.
+                stream().
+                filter(t -> t < 0).
+                map(t -> t * t).
+                reduce(Math::addExact).
+                orElseThrow());
+    }
 
     // S4: poziflerin kuplerinden max yazdiralim
 
+    public static void pozKupleri(List<Integer> intL) {
+        System.out.println(intL.
+                stream().
+                filter(t -> t > 0).
+                map(t -> t * t * t).
+                reduce(Math::max).
+                orElseThrow());
+
+    }
 }
--- src/arrays/mayis8/C5_Method_Referance_String.java
+++ src/arrays/mayis8/C5_Method_Referance_String.java
@@ -1,19 +1,39 @@
 package arrays.mayis8;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class C5_Method_Referance_String {
-    List<String> l= Arrays.asList("Elma", "Muz", "Portakal", "Cilek", "Limon");
+    public static void main(String[] args) {
+        List<String> l = Arrays.asList("Elma", "Muz", "Portakal", "Cilek", "Limon","Erik");
+        buyukHarfleriYaz(l);
+        System.out.println();
+        uzunluklaraGoreSirala(l);
+        System.out.println();
+        eIleBasl(l);
+    }
 
 
-}
-// S1: Tum harfleri buyuk harf ile aralarinda bosluk birakarak yazdiralim
+    // S1: Tum harfleri buyuk harf ile aralarinda bosluk birakarak yazdiralim
+    public static void buyukHarfleriYaz(List<String> l) {
+        l.stream().map(t->t.toUpperCase()).forEach(t-> System.out.print(t+ " "));
 
+
+    }
 
 //S2: Stringleri uzunluklarina gore siralayip yaziniz
+    public static void uzunluklaraGoreSirala(List<String> l){
+        System.out.println(l.stream().sorted(Comparator.comparing(t -> t.length())).collect(Collectors.toList()));
+
+    }
 
 
 //S3: E ILE Baslayanlari yazdiralim
+    public static void eIleBasl(List<String> l){
+        System.out.println(l.stream().filter(t -> t.startsWith("E")).collect(Collectors.toList()));
 
+    }
 
+}
